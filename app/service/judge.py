0b228e60759473946a0cf0631c48af77b5e7c4ad
Edupizzol <edupizzol16@gmail.com>
2026-01-03 23:25:31 -0300

    adicionando a service de julgar os codigos

--- app/service/judge.py
+++ app/service/judge.py
@@ -1,1 +1,31 @@
-asd
+def normalize(output: str) -> list[str]:
+    return [line.rstrip() for line in output.splitlines()]
+
+def judge(run_result: dict, expected_output: str) -> dict:
+    status = run_result["status"]
+
+    if status == "TLE":
+        return{
+            **run_result,
+            "verdict": "TLE"
+        }
+    
+    if status == "RUNTIME_ERROR":
+        return{
+            **run_result,
+            "verdict": "RUNTIME_ERROR"
+        }
+    
+    expected = normalize(expected_output)
+    user_output= normalize(run_result["stdout"])
+
+    if user_output == expected:
+        return{
+            **run_result,
+            "verdict": "AC"
+        }
+    else:
+        return{
+            **run_result,
+            "verdict": "WC"
+        }
