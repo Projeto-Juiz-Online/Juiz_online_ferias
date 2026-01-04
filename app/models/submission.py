a734c210e17fbd683f5d3611858d0fd03c50766b
Edupizzol <edupizzol16@gmail.com>
2026-01-04 17:51:43 -0300

    teste q talvez de muito problema

--- app/models/submission.py
+++ app/models/submission.py
@@ -9,7 +9,7 @@ class Submission(db.Model):
     status = db.Column(db.String(30), nullable = False, default="PENDING")
     language = db.Column(db.String(30), nullable = False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    problem_id = db.Column(db.Integer, db.ForeignKey('problems.id', ondelete='CASCADE'), nullable=False)
+    problem_id = db.Column(db.Integer, db.ForeignKey('problems.id'), nullable=False)
     submitted_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc)) #essa e a data da submissao
     code = db.Column(db.Text, nullable = False)    #esse e o codigo do problema
     user = db.relationship("User", backref="submissions") #essas duas ultimas linhas e so pra facilitar a escrever o codigo
