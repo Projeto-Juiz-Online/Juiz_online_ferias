203bae8eeb3f3a50dbfd351e26c530d6c2d266cd
Edupizzol <edupizzol16@gmail.com>
2026-02-14 00:26:30 -0300

    Feat: criando service de Contest

---
+++ app/service/contest_service.py
@@ -0,0 +1,56 @@
+from app.service.database import db
+from app.model.user import User
+from app.model.problem import Problem
+from app.model.contest import Contest
+
+def create_contest(name,description,start_time,end_time,creator_id):
+
+    contest = Contest(name=name, description=description, start_time=start_time, end_time=end_time, creator_id=creator_id)
+
+    db.session.add(contest)
+    db.session.commit()
+    return contest
+
+def delete_contest(contest_id):
+
+    contest = Contest.query.get(contest_id)
+    db.session.delete(contest)
+    db.session.commit()
+    return contest
+
+def add_user(user_id, contest_id):
+
+    contest = Contest.query.get(contest_id)
+    user = User.query.get(user_id)
+
+    contest.users.append(user)
+    db.session.commit()
+    return user
+
+def remove_user(user_id, contest_id):
+
+    contest = Contest.query.get(contest_id)
+    user = User.query.get(user_id)
+
+    contest.users.remove(user)
+    db.session.commit()
+    return user
+
+def add_problem(problem_id, contest_id):
+
+    contest = Contest.query.get(contest_id)
+    problem = Problem.query.get(problem_id)
+
+    contest.problems.append(problem)
+    db.session.commit()
+    return problem
+
+def remove_problem(problem_id, contest_id):
+
+    contest = Contest.query.get(contest_id)
+    problem = Problem.query.get(problem_id)
+
+    contest.problems.remove(problem)
+    db.session.commit()
+    return problem
+
